bfcc170632a6aa39e5fd4d40f1ff16e53556bfc9
Alexey Radkov <alexey.radkov@gmail.com>
2023-09-21 17:38:57 +0400

    correctly free memory returned by the C handler

--- NgxExport/Healthcheck.hs
+++ NgxExport/Healthcheck.hs
@@ -159,12 +159,12 @@ mkHttpsManager us hname = do
                                     then do
                                         v <- peek pv
                                         (fromIntegral -> l) <- peek pl
-                                        v' <- B.unsafePackCStringLen (v, l)
-                                        free pv
+                                        (T.decodeUtf8 -> !v') <-
+                                            B.unsafePackCStringLen (v, l)
+                                        free v
                                         return $ filter (not . T.null)
                                                $ map (T.takeWhile (/= ':'))
-                                               $ T.split (== ',')
-                                               $ T.decodeUtf8 v'
+                                               $ T.split (== ',') v'
                                     else terminateWorkerProcess $
                                         "Failed to get servers in upstream " ++
                                             T.unpack ps ++ "!"
